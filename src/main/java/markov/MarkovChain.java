da5d71cd0dc49d6bcc0868db20db50dee902a0f6
Milieuuh <35493286+Milieuuh@users.noreply.github.com>
2020-12-15 14:49:38 +0100

    Ajout Commentaires pour la classe MarkovApp

--- src/main/java/markov/MarkovChain.java
+++ src/main/java/markov/MarkovChain.java
@@ -5,6 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+/**
+ * Class MarkovChain
+ * permet de traiter le chaine
+ */
 public class MarkovChain {
 	
 	/** Markov chain ngram parameter (contiguous sequence of words) */
